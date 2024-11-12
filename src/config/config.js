2909666c805c2df5111961de50b2e9d23088b159
Xitlaly2003 <talyfel.2003@proton.me>
2024-11-12 01:18:26 -0600

    se actualizo base dedatos

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://tony:9485259@seguridadapp.w0qxhpc.mongodb.net/?retryWrites=true&w=majority&appName=SeguridadApp',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://user:Taly123@cluster0.bgpk3zd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   };
   
