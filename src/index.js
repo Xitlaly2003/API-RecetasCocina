7b6848438a9ff6362e278ff17f9c054feec73277
Xitlaly2003 <talyfel.2003@proton.me>
2024-11-12 00:59:54 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 const db = require('./models/Db.model'); // Conexión a la base de datos
 
 dotenv.config();
-app.listen(3000, 'o.o.o.o', () =>{
+app.listen(3000, '0.0.0.0', () =>{
     console.log('Servidor corriendo en http://0.0.0.0:3000');
 })
